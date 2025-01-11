e19e9ea0f313cf90f722ccd3d48fd256c1cbb067
Dishantkanwar <104606853+Dishantkanwar@users.noreply.github.com>
2025-01-11 14:08:50 +0530

    Add files via upload

---
+++ admin/inc/header.php
@@ -0,0 +1,46 @@
+<div class="container-fluid bg-primary text-white p-3 d-flex align-items-center justify-content-between sticky-top" >
+    
+       <h2 class="mb-0 fw-bold" id="h22"><a class="navbar-brand me-4" href="dashboard.php"><img class="logo-brand" src="image/logo/logo1.png"></img></a>Snow World Tour & Travel</h2>
+       <a href="out.php" class="btn btn-light btn-sm" >Logout</a>
+    </div>
+    <div class="col-lg-2 boad  bg-warning " id="dashboard-menu" >
+        <nav class="navbar navbar-expand-lg navbar-light  ">
+            <div class="container-fluid flex-lg-column align-items-stretch">
+                <h3 class="mt-2 text-black fw-bold">Admin Panel</h3>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-column  align-items-stretch mt-2"  id="adminDropdown">
+                       <ul class="nav nav-pills flex-column t fw-bold">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="dashboard.php">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="packages.php">Packages</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="user.php">User</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="room.php">Booking</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="feature_facilities.php">Feature & Facilities</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="user_queries.php">User Queries</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="carousel.php">Carousel</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="settings.php">Setting</a>
+                            </li>
+                        </ul>
+                
+                                        
+                </div>
+            </div>
+        </nav>
+
+    </div>
